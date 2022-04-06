d713d62cde70688bc01c5c808c0e4e7265149a48
kitdi <kitdi@example.com>
2022-04-06 18:41:20 +0900

    * ApiAdapterのViewHolderに住所の表示を追加

--- app/src/main/java/jp/techacademy/keita/doi/apiapp/ApiAdapter.kt
+++ app/src/main/java/jp/techacademy/keita/doi/apiapp/ApiAdapter.kt
@@ -54,6 +54,8 @@ class ApiAdapter(private val context: Context): RecyclerView.Adapter<RecyclerVie
         val rootView : ConstraintLayout = view.findViewById(R.id.rootView)
         // レイアウトファイルからidがnameTextViewのCTextViewオブジェクトを取得し、代入
         val nameTextView: TextView = view.findViewById(R.id.nameTextView)
+        // レイアウトファイルからidがnameTextViewのCTextViewオブジェクトを取得し、代入
+        val addressTextView: TextView = view.findViewById(R.id.addressTextView)
         // レイアウトファイルからidがimageViewのImageViewオブジェクトを取得し、代入
         val imageView: ImageView = view.findViewById(R.id.imageView)
         // レイアウトファイルからidがfavoriteImageViewのImageViewオブジェクトを取得し、代入
@@ -89,6 +91,8 @@ class ApiAdapter(private val context: Context): RecyclerView.Adapter<RecyclerVie
             }
             // nameTextViewのtextプロパティに代入されたオブジェクトのnameプロパティを代入
             nameTextView.text = data.name
+            // addressTextViewのtextプロパティに代入されたオブジェクトのaddressプロパティを代入
+            addressTextView.text = data.address
             // Picassoライブラリを使い、imageViewにdata.logoImageのurlの画像を読み込ませる
             Picasso.get().load(data.logoImage).into(imageView)
             // 白抜きの星マークの画像を指定
